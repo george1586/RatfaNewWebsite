532a92e5ae7b00ab27697ca7e657e53e5264c76b
george1586 <george_ichim101@yahoo.com>
2026-05-03 15:52:09 +0300

    posthog changed to eu

--- RatfaNewWebsite/client/src/lib/analytics.js
+++ RatfaNewWebsite/client/src/lib/analytics.js
@@ -1,7 +1,7 @@
 import posthog from 'posthog-js';
 
 posthog.init('phc_qXLkPzb68QAAvD9UCtWPsuUdeLFB9krx8uLTqAD5dZvn', {
-    api_host: 'https://us.i.posthog.com',
+    api_host: 'https://eu.i.posthog.com',
     capture_pageview: false,
     capture_pageleave: true,
     persistence: 'localStorage',
